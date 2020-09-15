d9f86ee60aa4787fa8f4db83753c7e3c7cbb2d4e
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-09-14 19:35:48 -0700

    test batch data

--- tests/test_encoder_decoder.py
+++ tests/test_encoder_decoder.py
@@ -2,14 +2,26 @@
 
 
 import unittest
+
+import torch
+
 from src.encoder_decoder import EncoderDecoder
 from src.train import train_net
 
+
 class TestEncoderDecoder(unittest.TestCase):
 
+    def test_batch_data(self):
+        from src.data_utils import batch
+        data = torch.randn(6, 3, 16, 16) * 100
+        batch_size = 2
+        n_batches = data.shape[0] // batch_size
+        batched_data_shape_expected = torch.Size([n_batches, batch_size] + list(data.shape[1:]))
+        batched_data = batch(data, batch_size=2)
+        self.assertTrue(batched_data.shape, batched_data_shape_expected)
+
     def test_encoder_decoder(self):
-        import torch
-        x = torch.randn(4, 3, 16, 16)*100
+        x = torch.randn(4, 3, 16, 16) * 100
         c = EncoderDecoder(img_size=x.shape[-2:], embedding_size=2)
         y1 = c.forward(x)
         train_net(net=c, data=x, epochs=10, batch_size=2, verbose=True, lr=.0001, save_best_net=False)
