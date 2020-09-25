588b56b5876e950d0a0f34082b522443d2ef55a8
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-09-25 03:52:34 -0700

    work

--- src/encoder_decoder.py
+++ src/encoder_decoder.py
@@ -11,23 +11,40 @@ from src.cnn import CNN
 
 
 class EncoderDecoder(nn.Module):
-    def __init__(self, img_size=(256, 256), embedding_size=64):
+    def __init__(self, img_size=(256, 256), embedding_size=4, cnn_shape=None):
         super(EncoderDecoder, self).__init__()
 
         self.net = nn.Sequential()
 
-        # round down to next highest power of 2
-        self.img_size = (2**int(log2(img_size[0])), 2**int(log2(img_size[1])))
-        self.embedding_size = 2 ** int(log2(embedding_size))
+        # # round down to next highest power of 2
+        # self.img_size = (2**int(log2(img_size[0])), 2**int(log2(img_size[1])))
+        assert img_size[0]%2 == 0
+        assert img_size[1]%2 == 0
+        self.img_size = img_size
+        self.embedding_size = 2**int(log2(embedding_size))
+        print(f"embedding size: {self.embedding_size}")
 
-        self.encoder_shape = self.get_net_shape(net_type='encoder')
+        if cnn_shape == None:
+            cnn_shape = self.get_net_shape('encoder')
+        self.encoder_shape = cnn_shape
         print(f"encoder shape: {self.encoder_shape}")
-        encoder = CNN(shape=self.encoder_shape)
+        encoder = CNN(shape=self.encoder_shape, stride=2)
         self.net.add_module('encoder', encoder)
 
-        self.decoder_shape = self.get_net_shape(net_type='decoder')
+
+        # linear1 = nn.Linear(img_size[-1], img_size[-1] // compression_factor)
+        # self.net.add_module('linear1', linear1)
+        # # linear2 = nn.Linear(img_size[-1] // 2, img_size[-1] // 4)
+        # # self.net.add_module('linear2', linear2)
+        # #
+        # # linear3 = nn.Linear(img_size[-1] // 4, img_size[-1] // 2)
+        # # self.net.add_module('linear3', linear3)
+        # linear4 = nn.Linear(img_size[-1] // compression_factor, img_size[-1])
+        # self.net.add_module('linear4', linear4)
+
+        self.decoder_shape = self.get_net_shape('decoder')
         print(f"decoder shape: {self.decoder_shape}")
-        decoder = CNN(shape=self.decoder_shape)
+        decoder = CNN(shape=self.decoder_shape, stride=2)
         self.net.add_module('decoder', decoder)
 
     def forward(self, x: Tensor):
@@ -35,11 +52,13 @@ class EncoderDecoder(nn.Module):
 
     # Helper for __init__
     def get_net_shape(self, net_type: str) -> Tuple:
+
         net_shape = []
-        layer_size = self.img_size[-1]
-        while layer_size >= self.embedding_size:
-            net_shape.append(layer_size)
-            layer_size //= 2
+        next_layer_size = self.img_size[-1]
+        assert next_layer_size >= self.embedding_size
+        while next_layer_size >= self.embedding_size:
+            net_shape.append(next_layer_size)
+            next_layer_size //= 2
         if net_type == 'decoder':
             net_shape.reverse()
         return tuple(net_shape)
--- tests/test_encoder_decoder.py
+++ tests/test_encoder_decoder.py
@@ -12,18 +12,47 @@ from src.train import train_net
 
 class TestEncoderDecoder(unittest.TestCase):
 
+    # @unittest.skip
     def test_train_encoder_decoder_sunsets(self):
         from src.data_utils import get_image_data
-        from src.image_functions import show_image
-        image_data = get_image_data(n=16, img_size=(128, 128))
-        c = EncoderDecoder(img_size=image_data.shape[-2:], embedding_size=64)
+        from src.image_functions import show_image, show_images
+        import time
+        image_data = get_image_data(n=256, img_size=(512, 512))
+        c = EncoderDecoder(img_size=image_data.shape[-2:], embedding_size=128, cnn_shape=None)
+        show_image(image_data[0])
+        time.sleep(3)
         y1 = c.forward(image_data[:1])
         show_image(y1[0])
-        train_net(net=c, data=image_data, epochs=5, batch_size=4, verbose=True, lr=1e-6, save_best_net=False)
+        train_net(net=c, data=image_data, epochs=800, batch_size=16, verbose=True, lr=1e-3, save_best_net=False)
+        y = c.forward(image_data[:1])
+        show_image(y[0])
+        time.sleep(3)
+        show_images(image_data[:2])
+        embedding1 = c.net[:2](image_data[:1])
+        embedding2 = c.net[:2](image_data[1:2])
+        decoded = c.net[2:](embedding1*embedding2)
+        show_image(decoded[0])
+        time.sleep(3)
+        rand_y = c.forward(torch.randn(y[:1].shape)*100)
+        time.sleep(3)
+        show_image(rand_y[0])
+        self.assertTrue(torch.sum(abs(rand_y[0] - image_data[0])) > torch.sum(abs(y[0] - image_data[0])))
+
+    @unittest.skip
+    def test_train_encoder_decoder_sunset(self):
+        from src.data_utils import get_image_data
+        from src.image_functions import show_image
+        image_data = get_image_data(n=1, img_size=(16, 16))
+        c = EncoderDecoder(img_size=image_data.shape[-2:], embedding_size=16)
+        show_image(image_data[0])
+        y1 = c.forward(image_data[:1])
+        show_image(y1[0])
+        train_net(net=c, data=image_data, epochs=50, batch_size=1, verbose=True, lr=1e-5, save_best_net=False)
         y = c.forward(image_data[:1])
         show_image(y[0])
         self.assertTrue(image_data is not None)
 
+    @unittest.skip
     def test_train_encoder_decoder(self):
         x = torch.randn(4, 3, 16, 16) * 100
         c = EncoderDecoder(img_size=x.shape[-2:], embedding_size=2)
@@ -34,6 +63,7 @@ class TestEncoderDecoder(unittest.TestCase):
         y2_beats_y1 = torch.sum(abs(y2 - x)) < torch.sum(abs(y1 - x))
         self.assertTrue(y2_beats_y1)
 
+    @unittest.skip
     def test_batch_data(self):
         from src.data_utils import batch
         data = torch.randn(6, 3, 16, 16) * 100
@@ -43,6 +73,7 @@ class TestEncoderDecoder(unittest.TestCase):
         batched_data = batch(data, batch_size=2)
         self.assertTrue(batched_data.shape, batched_data_shape_expected)
 
+    @unittest.skip
     def test_get_and_show_image(self):
         from src.data_utils import get_image_data
         from src.image_functions import show_image
@@ -50,12 +81,17 @@ class TestEncoderDecoder(unittest.TestCase):
         show_image(image_data[0])
         self.assertTrue(image_data is not None)
 
+    @unittest.skip
     def test_get_and_show_images(self):
         from src.data_utils import get_image_data
-        from src.image_functions import show_images_from_vec
+        from src.image_functions import show_images
         image_data = get_image_data(n=10)
-        show_images_from_vec(image_data[:4])
+        show_images(image_data[:4])
         self.assertTrue(image_data is not None)
+
+    @unittest.skip
+    def testPass(self):
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
